9ef27873aedeec68fedfb3a9b2503ddca1395bf2
ayushn2 <iayush.n2@gmail.com>
2024-10-28 00:13:13 +0530

    Developed a Custom Decoder for TCP Transport

--- p2p/message.go
+++ p2p/message.go
@@ -1,7 +1,10 @@
 package p2p
 
+import "net"
+
 // Message holds any or arbitrary data that is being send over each transport  between two nodes in the network
 type Message struct{
+	From net.Addr
 	Payload []byte
 
 }
--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -96,6 +96,8 @@ func (t *TCPTransport) handleConn(conn net.Conn){
 			continue
 		}
 
+		msg.From = conn.RemoteAddr()
+
 		fmt.Printf("message : %v\n",msg)
 	}
 
